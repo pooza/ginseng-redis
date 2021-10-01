43fdc9f21a64660f5eccf59873f0e33424e8439b
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-10-01 19:15:00 +0900

    #23

--- test/service_test.rb
+++ test/service_test.rb
@@ -5,6 +5,17 @@ module Ginseng
         @service = Service.new
       end
 
+      def test_key?
+        key = SecureRandom.hex
+        assert_false(@service.key?(key))
+        @service[key] = 1
+        assert(@service.key?(key))
+        @service[key] = nil
+        assert(@service.key?(key))
+        @service.del(key)
+        assert_false(@service.key?(key))
+      end
+
       def test_edit
         assert_equal(@service.set(__dir__, '一兆度の炎'), 'OK')
         assert_equal(@service.get(__dir__), '一兆度の炎')
